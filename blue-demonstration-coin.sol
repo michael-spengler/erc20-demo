e48ca2baa605ddb2c949d844e5f06de3d720fef8
michael spengler <43786652+michael-spengler@users.noreply.github.com>
2023-05-24 16:05:57 +0200

    Update blue-demonstration-coin.sol

--- blue-demonstration-coin.sol
+++ blue-demonstration-coin.sol
@@ -1,6 +1,7 @@
-// This Smart Contract defines the cryptocurrency named Blue Demonstration Coin (BDC)
-// Information about this project can be found via https://www.bluedemonstration.com
-// Please make sure to use the right smart contract address on the correct blockchain / network whenever you use the Blue Demonstration Coin
+// This Smart Contract defines the cryptocurrency named Blue Demonstration Coin (BDC)  
+// Information about this project can be found via https://www.bluedemonstration.com  
+// Please make sure to use the right smart contract address & ensure you are connected with the Arbitrum Network (https://chainlist.org/chain/42161) whenever you use the Blue Demonstration Coin 
+// We decided to deploy the BDC on Arbitrum (https://arbitrum.io) in order to save gas fees for everyone who interacts with the BDC  
 
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.2; // later compiler versions led to strange errors wrt deployments via remix.ethereum.org 
