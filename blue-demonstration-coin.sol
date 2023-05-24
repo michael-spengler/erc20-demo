48aeaf0400d775e07222ef24de320233f6240c16
michael spengler <43786652+michael-spengler@users.noreply.github.com>
2023-05-24 15:41:46 +0200

    Update blue-demonstration-coin.sol

--- blue-demonstration-coin.sol
+++ blue-demonstration-coin.sol
@@ -1,9 +1,9 @@
 // This Smart Contract defines the cryptocurrency named Blue Demonstration Coin (BDC)
 // Information about this project can be found via https://www.bluedemonstration.com
-// Please always check that you are using the right smart contract address - e.g. via https://etherscan.io/
+// Please always check that you are using the right smart contract address - e.g. via https://etherscan.io
 
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity 0.8.2; // later compiler versions led to strange errors wrt deployments via remix.ethereum.org 
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
